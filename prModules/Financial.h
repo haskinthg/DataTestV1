01772f9a24b746ea2ecd448582ed7c81f02b9144
haskinthg <80718822+haskinthg@users.noreply.github.com>
2021-07-05 19:51:38 +0300

    Add files via upload
    first commit

---
+++ prModules/Financial.h
@@ -0,0 +1,15 @@
+﻿#pragma once
+#include<string>
+class Financial
+{
+public:
+	double profit; // чистая прибыль
+	int rentability; // рентабельность
+	double gain; // выручка
+
+	Financial();
+	Financial(double, int, double);
+
+	std::string getInfo();
+};
+
